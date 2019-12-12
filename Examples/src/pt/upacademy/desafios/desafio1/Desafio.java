0c53de32e3c8bc222a1e37713fb3943b796f7bb4
Goncalo Amaro <goncaloacorp@gmail.com>
2019-12-12 15:32:51 +0000

    resolve 1 2

--- Examples/src/pt/upacademy/desafios/desafio1/Desafio.java
+++ Examples/src/pt/upacademy/desafios/desafio1/Desafio.java
@@ -2,23 +2,45 @@ package pt.upacademy.desafios.desafio1;
 
 public class Desafio {
 
+	private int count;
+	private int big3 = 0;
+	private int big5 = 0;	
+	
 	public Desafio(int i) {
-		// TODO Auto-generated constructor stub
+		this.count = i;
 	}
 
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		for (int i = 1; i <= count; i++) {
+			if (i%3 == 0) {
+				big3++;
+				System.out.println("test_01");
+				System.out.println(big3);				
+			}
+			if (i%5 == 0) {
+				big5++;
+				System.out.println("test_02");
+				System.out.println(big5);				
+			}
+		}
+		System.out.println("big3 é");
+		System.out.println(big3);
+		System.out.println("big5 é");
+		System.out.println(big5);		
 	}
 
 	public int getBig3Count() {
-		// TODO Auto-generated method stub
-		return 0;
+		System.out.println("estou a funcionar 2");
+		return big3;
+		
+//		return 0;
 	}
 
 	public int getBig5Count() {
-		// TODO Auto-generated method stub
-		return 0;
+		System.out.println("estou a funcionar 3");
+		return big5;
+		
+//		return 0;
 	}
 
 }
--- Examples/src/pt/upacademy/desafios/desafio2/Desafio.java
+++ Examples/src/pt/upacademy/desafios/desafio2/Desafio.java
@@ -1,10 +1,45 @@
 package pt.upacademy.desafios.desafio2;
 
+import java.util.*;
+
 public class Desafio {
 
+	public static String phrase;
+	private String singleWords[];
+	private StringBuilder seqOrdenada;
+	
 	public static String order(String string) {
-		// TODO Auto-generated method stub
+		
+		phrase = string;
+		
+		String singleWords[] = phrase.split("\\p{Space}", -1);
+		
+		List<String> singleWords2 = Arrays.asList(singleWords);
+		
+			for (int i = 0; i < singleWords.length; i++) {
+				
+				String s = singleWords[i];
+				String clean = s.replaceAll("\\D+",""); 
+				int num = Integer.parseInt(clean);
+				System.out.println(num);
+				
+				String sSeguinte = singleWords[+i];
+				String cleanSeguinte = sSeguinte.replaceAll("\\D+",""); 
+				int num2 = Integer.parseInt(cleanSeguinte);
+				System.out.println(num2);
+				
+	
+				if (num > num2) {					
+					System.out.println(i);
+					System.out.println(+i);
+					Collections.swap(singleWords2, i, +i); 
+				}
+				
+			}		
+		
 		return null;
 	}
+	
+	
 
 }
